f8439e91220298e4560d94ae3831ab90d3f64bb6
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2025-12-13 23:26:42 -0300

    Excluir antigo animacoes.js

--- animacoes.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("img.icon").forEach(icon => {
-    icon.addEventListener("click", function (e) {
-        e.preventDefault();
-        const link = this.parentElement.href;
-        this.classList.add("clicando");
-        setTimeout(() => {
-            window.open(link, "_blank");
-            this.classList.remove("clicando");
-        }, 220)});});
